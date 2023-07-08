0d4c36e7152ea90be5f37b1d491af829bd8a221b
Vlados117 <vlados117@gmail.com>
2023-07-08 21:50:46 +0300

    new git

--- resources/js/components/Applications/New/ApplicationsComponent.vue
+++ resources/js/components/Applications/New/ApplicationsComponent.vue
@@ -3,7 +3,9 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Заявки</div>
+                    <div class="card-header">
+                        Заявки
+                    </div>
                     <!-- 
                     <div class="card-body">
                         I'm an example component.
